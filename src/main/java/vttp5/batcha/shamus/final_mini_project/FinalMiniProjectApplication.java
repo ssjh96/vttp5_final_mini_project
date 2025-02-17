733543e8f7f1d930c161c5eab22f4700fe52e61a
ssjh96 <ssjh96@outlook.com>
2025-02-17 14:43:07 +0800

    initial dependencies

---
+++ src/main/java/vttp5/batcha/shamus/final_mini_project/FinalMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package vttp5.batcha.shamus.final_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalMiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp5/batcha/shamus/final_mini_project/FinalMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp5.batcha.shamus.final_mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
